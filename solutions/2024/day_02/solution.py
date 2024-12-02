1a88ad5447cdad3aa9c4e71b72d34b78d88497c9
Havish Rani <hrani2@illinois.edu>
2024-12-02 10:52:28 -0600

    part 1 done

--- solutions/2024/day_02/solution.py
+++ solutions/2024/day_02/solution.py
@@ -2,16 +2,38 @@
 
 # puzzle prompt: https://adventofcode.com/2024/day/2
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
 
 
-class Solution(TextSolution):
+class Solution(StrSplitSolution):
     _year = 2024
     _day = 2
 
     # @answer(1234)
     def part_1(self) -> int:
-        pass
+        safe_count = 0 
+        for line in self.input: 
+            report = line.split()
+            safe_dec = False
+            safe_inc = False
+            report = list(map(int,report))
+            if all((report[i] > report[i+1]) & (abs(report[i] - report[i+1]) <= 3) for i in range(len(report)-1)):
+                safe_dec = True
+                print("increasing list")
+            if all((report[i] < report[i+1]) & (abs(report[i] - report[i+1]) <= 3) for i in range(len(report)-1)):
+                safe_inc = True
+                print("decreasing list")
+
+            # for i in range(len(report)-1): 
+            #     if (abs(report[i] - report[i+1]) > 3): 
+            #         safe = False
+            #     # if (report[i])
+            #     # print(i)
+            if safe_dec or safe_inc: safe_count += 1
+        return safe_count
+            # print(report)
+            
+        
 
     # @answer(1234)
     def part_2(self) -> int:
