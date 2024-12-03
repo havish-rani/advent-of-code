6bd69caa9b867f8f69ff67bc296c8f1619253dcb
Havish Rani <hrani2@illinois.edu>
2024-12-03 08:46:23 -0600

    started day 3

---
+++ solutions/2024/day_03/solution.py
@@ -0,0 +1,22 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/3
+
+from ...base import StrSplitSolution, answer
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 3
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
